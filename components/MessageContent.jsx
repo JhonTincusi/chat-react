6291a0bcd1c30428b92f9bb129d90a4f03527c6f
Jhon Tincusi <ajhonjtc@gmail.com>
2023-08-14 18:58:53 -0500

    commit

--- components/MessageContent.jsx
+++ components/MessageContent.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { auth } from '../firebase';
 import MessageHeader from './MessageHeader'; // ¡Asegúrate de importar correctamente!
